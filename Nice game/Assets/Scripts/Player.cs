19da3627461c02b7e32fbe627b02fd75ef1a40dc
ialko007 <ialko007@edu.xamk.fi>
2025-12-02 09:05:23 +0200

    Player now can manage jump height

--- Nice game/Assets/Scripts/Player.cs
+++ Nice game/Assets/Scripts/Player.cs
@@ -7,18 +7,30 @@ public class Player : MonoBehaviour
     bool isRunning = false;
     Animator animator;
     Rigidbody2D rb;
+    PlayerInput input;
     public Canvas canvasToHide;
+
+    public float jumpImpulse = 20.0f;
 
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
         rb = gameObject.GetComponent<Rigidbody2D>();
+        input = gameObject.GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
         animator.SetFloat("xVelocity", isRunning ? 1.0f : 0.0f);
+        if (input.actions["Jump"].inProgress)
+        {
+            rb.gravityScale = 1.0f;
+        }
+        else
+        {
+            rb.gravityScale = 3.0f;
+        }
     }
     
     void OnJump()
@@ -30,13 +42,13 @@ public class Player : MonoBehaviour
         }
         else
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, 1.5f);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, 1.0f);
             if (hit)
             {
                 Debug.Log("Hit object: " + hit.transform.name);
                 Debug.Log("On the ground - can jump!");
                 rb.linearVelocityY = 0.0f;
-                rb.AddForce(transform.up * 20.0f, ForceMode2D.Impulse);
+                rb.AddForce(transform.up * jumpImpulse, ForceMode2D.Impulse);
             }
             else
             {
